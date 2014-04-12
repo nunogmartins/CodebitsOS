49f44d697901253cc52fa76cb2f0092e66538a0c
Martin Naumann <martin.naumann@centralway.com>
2014-04-12 13:05:48 +0200

    Using the keyboard_handler stuff instead the inlined version

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,39 +1,12 @@
 #include "screen.h"
 #include "isr.h"
-
-int posX = 0;
-int posY = 0;
-
-void move(registers_t *regs) {
-
-    unsigned char scanCode = port_byte_in(0x60);
-
-    switch(scanCode) {
-        case 0xCD:                // RIGHT pressed
-            if(posX < 65) posX++;
-        break;
-        case 0xCB:                // LEFT pressed
-            if(posX > 0) posX--;
-        break;
-        case 0xD0:                // DOWN pressed
-            if(posY < 24) posY++;
-        break;
-        case 0xC8:                // UP pressed
-            if(posY > 0) posY--;
-        break;
-    }
-
-    clear();
-    setCursor(posX, posY);
-    print("Hello, Codebits");
-
-}
+#include "keyboard_handler.h"
 
 void main() {
     clear();
     install_idt();
     setup_isrs();
-    setup_irq_handler(1, move);
+    setup_irq_handler(KEYBOARD_IRQ, echoKey);
 
     __asm__ __volatile__ ("sti");
 
